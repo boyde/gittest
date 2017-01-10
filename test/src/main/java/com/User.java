0da92494ff7609256fe7c97e89cee0b0b74be7c6
chenbo <chenbo@gomeholdings.com>
2017-01-10 19:15:00 +0800

    master

--- test/src/main/java/com/User.java
+++ test/src/main/java/com/User.java
@@ -1,25 +1,5 @@
 package com;
 
 public class User {
-	private String name;
-	private String sex;
-	private String newdev;
-	private String newdev2;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
+	private String master;
 }
